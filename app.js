b6395911bd36129a22b0dd652c085a13e3f85164
Makohon Pavlo <makohon@Makohons-Mac-mini.local>
2026-03-12 16:12:01 +0200

    Added HTTP Server

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+const http = require("http");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    console.log("Appexoft");
+    res.end("Hello Appexoft");
+});
+
+server.listen(PORT, () => {
+    console.log(`server is listening on port ${PORT}`);
+});
